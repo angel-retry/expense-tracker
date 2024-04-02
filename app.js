56736c5d545c63bc67b2da9f89ded0891dbaebeb
a27657060 <a27657060@gmail.com>
2024-04-02 15:30:16 +0800

    feat: add view engine hbs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')

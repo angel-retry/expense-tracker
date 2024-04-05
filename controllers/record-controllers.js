607e8eb8819a0331eac7bdcf47e4654596d3531d
a27657060 <a27657060@gmail.com>
2024-04-05 12:57:47 +0800

    feat: add new-record page

--- controllers/record-controllers.js
+++ controllers/record-controllers.js
@@ -1,6 +1,14 @@
+const { Category } = require('../models')
+
 const recordControllers = {
   getRecords: (req, res, next) => {
     return res.render('records')
+  },
+  newRecordPage: (req, res, next) => {
+    Category.findAll({ raw: true })
+      .then(categories => {
+        return res.render('new-record', { categories })
+      })
   }
 }
 
--- routes/modules/record.js
+++ routes/modules/record.js
@@ -2,6 +2,7 @@ const express = require('express')
 const recordControllers = require('../../controllers/record-controllers')
 const router = express.Router()
 
+router.get('/new', recordControllers.newRecordPage)
 router.get('/', recordControllers.getRecords)
 
 module.exports = router

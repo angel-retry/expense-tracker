c9b3271ca73550fb8f7e75e6e0b106b57ddf44ed
a27657060 <a27657060@gmail.com>
2024-04-08 16:23:53 +0800

    feat: edit sequelize seed data

--- seeders/20240403064211-add-category-data.js
+++ seeders/20240403064211-add-category-data.js
@@ -20,6 +20,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Categories', null)
+    await queryInterface.bulkDelete('Categories', null)
   }
 }
